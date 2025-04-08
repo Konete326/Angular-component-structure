3724077ded2a948d75ed4b9ba9895d84d2d15210
S A M E E R <sameerakramsameerakram01@gmail.com>
2025-04-08 06:46:13 +0500

    Add Readme File

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'contact';
+  title = 'Assignment';
+  firstname: string = "";
+  lastname: string = "";
+  email: string = "";
+  password: string = "";
+  Confirmpassword: string = "";
+
+  show() {
+    alert(`First Name = ${this.firstname} Last Name = ${this.lastname} Email = ${this.email} Password = ${this.password} Confirm Password = ${this.Confirmpassword}`);
+  }
 }
